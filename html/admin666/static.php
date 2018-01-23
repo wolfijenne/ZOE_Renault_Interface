417b11fff08a3f2d16b8cb4e5ca8bd97bcd52ddb
Wolfgang Jenne <wolfi@spider.li>
2018-01-24 00:45:32 +0100

    Some Fixes and Cleanup

--- html/admin666/static.php
+++ html/admin666/static.php
@@ -7,7 +7,6 @@ $dbhost='localhost';
 $dbuser='zoe_user';
 $dbpass='d5Kmz77?';
 
-//
 $mysqli = new mysqli($dbhost, $dbuser, $dbpass, $db);
 $mysqli->set_charset("utf8");
 $openssl_key="2345gneekc6t3FRum43d%Dueb4icdtheRDCjrmfz3765jetckf";
